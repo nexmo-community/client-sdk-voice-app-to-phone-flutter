24bac44efb6ae95077a0fb374b75c9086ab2a0a1
igor.wojda <igor.wojda@gmail.com>
2021-03-01 14:46:01 +0100

    Update permissions request

--- lib/main.dart
+++ lib/main.dart
@@ -6,16 +6,6 @@ void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
 
-  MyApp() {
-    requestPermissions();
-  }
-
-  Future<void> requestPermissions() async {
-    Map<Permission, PermissionStatus> statuses = await [
-      Permission.microphone
-    ].request();
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -60,7 +50,7 @@ class _LoginWidgetState extends State<LoginWidget> {
   }
 
   Future<void> _loginUser() async {
-    String token = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2MTQzNDE0MzQsImp0aSI6ImExMTBjNjEwLTc4MmItMTFlYi1hNmQyLTg5NmQ2NGRjMDJlOSIsImV4cCI6MTYxNDQyNzgzNCwiYWNsIjp7InBhdGhzIjp7Ii8qL3VzZXJzLyoqIjp7fSwiLyovY29udmVyc2F0aW9ucy8qKiI6e30sIi8qL3Nlc3Npb25zLyoqIjp7fSwiLyovZGV2aWNlcy8qKiI6e30sIi8qL2ltYWdlLyoqIjp7fSwiLyovbWVkaWEvKioiOnt9LCIvKi9hcHBsaWNhdGlvbnMvKioiOnt9LCIvKi9wdXNoLyoqIjp7fSwiLyova25vY2tpbmcvKioiOnt9LCIvKi9sZWdzLyoqIjp7fX19LCJzdWIiOiJBbGljZSIsImFwcGxpY2F0aW9uX2lkIjoiMTczYjg5N2EtNjlkMi00NjMyLTg4NjAtZTk2M2M2ZTkyNmY2In0.fCgJDRf48Pz-RIzl0OMyUJ4stG8lTlwUxCYMnMjUsP0AXaRBP_D3PqnhABZYh3cSsET0d3ZZhoAkKXUM4X1rdjB3baiKEidC069fBD8IFUQg5mA6_n6vnxl7fFv1btitrASo1i9kdjjtyig2-vgUj-zELyGeKhvsQAXptYflckAMycyiWSF9-R-HN9ewcjIXzxwt-wa7atgKWZ2hYBp6g47Jv4keZh4S8qUudKb7SgkuleeSP_J4r3eNx1tq9VQICwOVxgy5ONFqaBjLkk7inE0rm7yKxT4SzXGx17YcON6sE3r5Uz19w7VxMN5aoceJJxcLdgjYOssrTDsIyc0GoA";
+    String token = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2MTQ2MDYyNzcsImp0aSI6IjQzZGZlZmYwLTdhOTQtMTFlYi04NzkzLWZkYzQwM2RiOGExNiIsImV4cCI6MTYxNDY5MjY3NywiYWNsIjp7InBhdGhzIjp7Ii8qL3VzZXJzLyoqIjp7fSwiLyovY29udmVyc2F0aW9ucy8qKiI6e30sIi8qL3Nlc3Npb25zLyoqIjp7fSwiLyovZGV2aWNlcy8qKiI6e30sIi8qL2ltYWdlLyoqIjp7fSwiLyovbWVkaWEvKioiOnt9LCIvKi9hcHBsaWNhdGlvbnMvKioiOnt9LCIvKi9wdXNoLyoqIjp7fSwiLyova25vY2tpbmcvKioiOnt9LCIvKi9sZWdzLyoqIjp7fX19LCJzdWIiOiJBbGljZSIsImFwcGxpY2F0aW9uX2lkIjoiMTczYjg5N2EtNjlkMi00NjMyLTg4NjAtZTk2M2M2ZTkyNmY2In0.XXQNJEQYg4Aco7W9QVnKB87E6ChKO-_lgEHexNHq3ty5NaRAHTTkHviI9xfHk7Eo1J7HPLGRq53f_z6OsF8tdkp7K1RIH50pxmJ7Qvf4eVfgD7za0xdVYN8UynSM3hWL6v3Q9W811sYHeYRSofURPukBcnrmypwP8FLq7ALazC4Km1aJMW6DPlP03HCv5HRBd5H1v3haR3G8GvlwNJ0mQRKAJi7fQfqAmvOPVrEGJ2XBv8gZ443Ypdl2J2Osh5FtoXizDv08dNR39G2PzbtF9TGV8Ehq0WX9fvu1sQ4mBHI6fPNEMmxPQPQqncAW-FMBzEPDvZfMOo5OPNcbhJS2tg";
 
     try {
       await platformMethodChannel
@@ -72,11 +62,19 @@ class _LoginWidgetState extends State<LoginWidget> {
 
   Future<void> _makeCall() async {
     try {
+      await requestPermissions();
+
       await platformMethodChannel
           .invokeMethod('makeCall');
     } on PlatformException catch (e) {
       print(e);
     }
+  }
+
+  Future<void> requestPermissions() async {
+    Map<Permission, PermissionStatus> statuses = await [
+      Permission.microphone
+    ].request();
   }
 
   Future<void> _endCall() async {
